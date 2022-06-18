701c3c9776ae9a0d1279e80deb7b880ebe672cc3
Lizhuyou <84826607@qq.com>
2022-06-19 00:02:14 +0800

    ä¼å®结构型

---
+++ structure/flyweight/flyweight.go
@@ -0,0 +1,115 @@
+package flyweight
+
+import "fmt"
+
+// 可以说是共享配置实例 享元模式的本质就是为了节约内存资源，有些相同的不变的属性可以共用
+
+const (
+	//TerroristDressType terrorist dress type
+	TerroristDressType = "tDress"
+	//CounterTerrroristDressType terrorist dress type
+	CounterTerrroristDressType = "ctDress"
+)
+
+var (
+	dressFactorySingleInstance = &dressFactory{
+		dressMap: make(map[string]dress),
+	}
+)
+
+type dress interface {
+	getColor() string
+}
+
+type dressFactory struct {
+	dressMap map[string]dress
+}
+
+func (d *dressFactory) getDressByType(dressType string) (dress, error) {
+	if d.dressMap[dressType] != nil {
+		return d.dressMap[dressType], nil
+	}
+
+	if dressType == TerroristDressType {
+		d.dressMap[dressType] = newTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+	if dressType == CounterTerrroristDressType {
+		d.dressMap[dressType] = newCounterTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+
+	return nil, fmt.Errorf("Wrong dress type passed")
+}
+
+func getDressFactorySingleInstance() *dressFactory {
+	return dressFactorySingleInstance
+}
+
+type terroristDress struct {
+	color string
+}
+
+func (t *terroristDress) getColor() string {
+	return t.color
+}
+
+func newTerroristDress() *terroristDress {
+	return &terroristDress{color: "red"}
+}
+
+type counterTerroristDress struct {
+	color string
+}
+
+func (c *counterTerroristDress) getColor() string {
+	return c.color
+}
+
+func newCounterTerroristDress() *counterTerroristDress {
+	return &counterTerroristDress{color: "green"}
+}
+
+type player struct {
+	dress      dress
+	playerType string
+	lat        int
+	long       int
+}
+
+func newPlayer(playerType, dressType string) *player {
+	dress, _ := getDressFactorySingleInstance().getDressByType(dressType)
+	return &player{
+		playerType: playerType,
+		dress:      dress,
+	}
+}
+
+func (p *player) newLocation(lat, long int) {
+	p.lat = lat
+	p.long = long
+}
+
+type game struct {
+	terrorists        []*player
+	counterTerrorists []*player
+}
+
+func newGame() *game {
+	return &game{
+		terrorists:        make([]*player, 1),
+		counterTerrorists: make([]*player, 1),
+	}
+}
+
+func (c *game) addTerrorist(dressType string) {
+	player := newPlayer("T", dressType)
+	c.terrorists = append(c.terrorists, player)
+	return
+}
+
+func (c *game) addCounterTerrorist(dressType string) {
+	player := newPlayer("CT", dressType)
+	c.counterTerrorists = append(c.counterTerrorists, player)
+	return
+}
