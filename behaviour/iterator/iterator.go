414504ca5d14c0f2fb7502e461526ca72bb5e5d4
Lizhuyou <84826607@qq.com>
2022-06-19 23:21:01 +0800

    ä¼add iterator

--- behaviour/iterator/iterator.go
+++ behaviour/iterator/iterator.go
@@ -1,2 +1,36 @@
 package iterator
 
+type people struct {
+	Name string
+	Age int
+}
+
+type iterator interface {
+	hasNext() bool
+	GetNext() *people
+
+}
+
+type PeopleIterator struct {
+	index int
+	people []*people
+}
+
+func (p *PeopleIterator)hasNext() bool{
+	if p.index < len(p.people){
+		return true
+	}
+	return false
+}
+
+func (p *PeopleIterator)GetNext() *people{
+	if p.hasNext(){
+		p.index=p.index+1
+		return p.people[p.index-1]
+	}
+	return nil
+}
+
+func (p *PeopleIterator)SetIterator(p1 []*people){
+	p.people = p1
+}
--- behaviour/iterator/iterator_test.go
+++ behaviour/iterator/iterator_test.go
@@ -1,1 +1,17 @@
 package iterator
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	p1 :=&people{Name:"张三",Age:18}
+	p2 :=&people{Name:"李四",Age:19}
+	p3 :=&people{Name:"王五",Age:18}
+	p :=&PeopleIterator{people:[]*people{p1,p2,p3}}
+	for p.hasNext(){
+		pp :=p.GetNext()
+		fmt.Printf("User is %+v\n", *pp)
+	}
+}
